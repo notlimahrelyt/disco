e3f7adf08e3b04f2c7285dab34a8fb533c5d8d97
Tyler Hamilton <tyler.hamilton@me.com>
2021-04-21 10:51:06 -0400

    Update rsync_to_NFS.sh

--- scripts/rsync_to_NFS.sh
+++ scripts/rsync_to_NFS.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
 
-LOG_DIR=/tmp/SYNC_ERROR.log
 BACKUP_DIR=/mnt/backup/
 IGNORE_LIST=/tmp/ignore.txt
 DATE="$(date +"%Y-%m-%d") $(date +"%H:%M:%S")"
+LOG_FILE=/tmp/RSYNC_LOG.log
 
 
 if [ ! -d $BACKUP_DIR ]; then
 
-echo "$DATE - Backup Directory was not found." >> $LOG_DIR
+echo "$DATE - BACKUP FAILED." >> $LOG_FILE
 
 else
+
+echo "$DATE - BACKUP STARTED." >> $LOG_FILE
 
 rsync -avzp --info=progress2 /mnt/data1  /mnt/backup/data1/  --exclude-from="$IGNORE_LIST" | tee /mnt/backup/data1/sync.log & 
 rsync -avzp --info=progress2 /mnt/data2  /mnt/backup/data2/  --exclude-from="$IGNORE_LIST" | tee /mnt/backup/data2/sync.log &
 rsync -avzp --info=progress2 /mnt/data3  /mnt/backup/data3/  --exclude-from="$IGNORE_LIST" | tee /mnt/backup/data3/sync.log &
+
+wait
+
+DATE="$(date +"%Y-%m-%d") $(date +"%H:%M:%S")"
+echo "$DATE - BACKUP COMPLETED." >> $LOG_FILE
+
 
 fi;
